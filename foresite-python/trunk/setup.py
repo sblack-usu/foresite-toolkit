033bf4141b254952e924ab7c8226e4280372237e
azaroth42 <azaroth42@1f899623-014f-0410-8101-6bd834d6e58a>
2008-06-09 13:11:32 +0000

    distutils support

---
+++ foresite-python/trunk/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup (name="foresite",
+       version= "0.9",
+       author = "Rob Sanderson",
+       author_email = "azaroth@liv.ac.uk",
+       license = "BSD",
+       description = 'OAI-ORE Library',
+       long_description =       """ """,
+       platforms = "Any Python 2.4 or later",
+       url = "http://code.google.com/p/foresite-toolkit/",
+       packages = ["foresite"])
