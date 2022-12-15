1fc0b1ac37c8ca5b59e4e2a36cea37a8de98e0de
Scott Black <sblack@cuahsi.org>
2022-12-15 09:13:53 -0700

    update version

--- foresite-python/trunk/setup.py
+++ foresite-python/trunk/setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = '2.0'
+version = '2.1'
 setup(name='foresite',
       version=version,
       description='Library for constructing, parsing, manipulating and serializing OAI-ORE Resource Maps',
